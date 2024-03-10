f096e33fb0974e9d31eee93341a060302329d302
adeniz <ahmetdenizguner@gmail.com>
2024-03-10 16:25:09 +0300

    feat: add basic gin server

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"adg/e-commerce/internal/api"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := gin.Default()
+	api.RegisterHandlers(r)
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	log.Println("Server is running...")
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
 }
