f6a258b544fbe6a6df75ae8107ca46c35cb45141
adeniz <ahmetdenizguner@gmail.com>
2024-03-10 16:53:52 +0300

    feat: add configuration

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,7 +1,31 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"adg/e-commerce/internal/config"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+	"strings"
+)
+
+var AppConfig = &config.Configuration{}
 
 func RegisterHandlers(r *gin.Engine) {
+
+	os.Setenv("ENV", "local")
+
+	env := os.Getenv("ENV")
+
+	if !strings.EqualFold(env, "") {
+		env = config.DefaultEnv
+	}
+	cfgFile := "../../config/app." + env + ".yaml"
+	AppConfig, err := config.GetAllConfigValues(cfgFile)
+	if err != nil {
+		log.Fatalf("Failed to read config file. %v", err.Error())
+	}
+
+	fmt.Println(AppConfig)
 
 }
---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+const (
+	DefaultEnv = "local"
+)
+
+var cfgReader *configReader
+
+type (
+	Configuration struct {
+		DatabaseSettings
+		JwtSettings
+		RedisSettings
+	}
+
+	DatabaseSettings struct {
+		DatabaseURI  string
+		DatabaseName string
+		Username     string
+		Password     string
+	}
+
+	JwtSettings struct {
+		SecretKey string
+	}
+	RedisSettings struct {
+		AddrUrI string
+	}
+
+	configReader struct {
+		configFile string
+		v          *viper.Viper
+	}
+)
+
+func GetAllConfigValues(configFile string) (configuration *Configuration, err error) {
+	newConfigReader(configFile)
+	if err = cfgReader.v.ReadInConfig(); err != nil {
+		fmt.Printf("Failed to read config file : %s\n", err)
+		return nil, err
+	}
+
+	err = cfgReader.v.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Failed to unmarshal yaml file to configuration struct : %s\n", err)
+		return nil, err
+	}
+
+	return configuration, err
+}
+
+func newConfigReader(configFile string) {
+	v := viper.GetViper()
+	v.SetConfigType("yaml")
+	v.SetConfigFile(configFile)
+	cfgReader = &configReader{
+		configFile: configFile,
+		v:          v,
+	}
+}
